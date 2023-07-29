8590fa8c2a3d56b07b09efce06ca9bf2b6c8a785
Eujenio Gonzalez <vegas-host@yandex.ru>
2023-07-29 18:04:57 +0300

    Created repass form

--- src/pages/repass-page/repass-page.tsx
+++ src/pages/repass-page/repass-page.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import Content from '../../components/content/content';
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../../const/router-const';
 
 function RepassPage() {
   document.title = 'Repass';
@@ -10,7 +12,17 @@ function RepassPage() {
     <>
       <Header/>
       <Content>
-        <h1 className='repass-test'>Repass Page</h1>
+        <div className="repass-page-content">
+          <div className="repass-page-title">Вспомнить пароль</div>
+          
+          <input className="input repass-page-input-login" type="text" placeholder="E-mail" autoFocus />
+          <input className="submit repass-page-submit-button" type="submit" value="Вспомнить" />
+
+          <div className="repass-page-links">
+            <Link className="dark-link repass-page-links-item" to={AppRoutes.Login}>Войти</Link>
+            <Link className="dark-link repass-page-links-item" to={AppRoutes.Register}>Регистрация</Link>
+          </div>
+        </div>
       </Content>
       <Footer />
     </>
