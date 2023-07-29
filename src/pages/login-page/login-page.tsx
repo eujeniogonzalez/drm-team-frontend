8b8c643ad7ca9724c97e4ebb72b980051646c950
Eujenio Gonzalez <vegas-host@yandex.ru>
2023-07-29 17:43:25 +0300

    Created login form

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import Content from '../../components/content/content';
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../../const/router-const';
 
 function LoginPage() {
   document.title = 'Login';
@@ -10,7 +12,18 @@ function LoginPage() {
     <>
       <Header/>
       <Content>
-        <h1 className='login-test'>Login Page</h1>
+        <div className="login-page-content">
+          <div className="login-page-title">Вход</div>
+          
+          <input className="input login-page-input-login" type="text" placeholder="E-mail" autoFocus />
+          <input className="input login-page-input-password" type="password" placeholder="Пароль" />
+          <input className="submit login-page-submit-button" type="submit" value="Войти" />
+
+          <div className="login-page-links">
+            <Link className="dark-link login-page-links-item" to={AppRoutes.Repass}>Вспомнить пароль</Link>
+            <Link className="dark-link login-page-links-item" to={AppRoutes.Register}>Регистрация</Link>
+          </div>
+        </div>
       </Content>
       <Footer />
     </>
