01833b774e324154eaedd2be68b02949dc2f3d5c
Eujenio Gonzalez <vegas-host@yandex.ru>
2023-08-09 18:03:22 +0300

    Created Loader component

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,21 @@
+import { LoaderColors, LoaderSizes } from '../../const/loader-const';
+import './loader.scss';
+import React from 'react';
+
+type LoaderProps = {
+  size: LoaderSizes;
+  color: LoaderColors;
+};
+
+function Loader({ size, color }: LoaderProps) {
+  return (
+    <div className={`${size} ${color}`}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Loader;
---
+++ src/const/loader-const.ts
@@ -0,0 +1,12 @@
+export enum LoaderSizes {
+  Micro = 'lds-ellipsis-micro',
+  Small = 'lds-ellipsis-small',
+  Medium = 'lds-ellipsis-medium',
+  Large = 'lds-ellipsis-large'
+};
+
+export enum LoaderColors {
+  White = 'lds-ellipsis-bg-white',
+  Grey = 'lds-ellipsis-bg-grey',
+  Main = 'lds-ellipsis-bg-main'
+};
