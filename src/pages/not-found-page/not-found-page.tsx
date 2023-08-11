51a4a511023ca836dac4add2fb48ee30d9e53572
Eujenio Gonzalez <vegas-host@yandex.ru>
2023-08-12 00:05:34 +0300

    Improved design of Not Found page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,25 +1,17 @@
-import './not-found-page.scss';
 import React from 'react';
 import Header from '../../components/header-components/header/header';
 import Footer from '../../components/footer/footer';
 import Content from '../../components/content/content';
-import { Link } from 'react-router-dom';
-import { AppRoutes } from '../../const/router-const';
+import Message from '../../components/message/message';
 
 function NotFoundPage() {
   document.title = 'Not Found';
+
   return (
     <>
       <Header />
       <Content>
-        <div className="not-found-message">
-          <p>
-            Sorry, this page not exists
-          </p>
-          <p>
-            <Link to={AppRoutes.Main} className='dark-link'>На главную</Link>
-          </p>
-        </div>
+        <Message message={'Такой страницы не существует'} />
       </Content>
       <Footer />
     </>
