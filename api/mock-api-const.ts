8c9e8c14ed37f35cd62a4b0eefe266907668cc7d
Eujenio Gonzalez <vegas-host@yandex.ru>
2023-08-15 20:26:25 +0300

    Added generating JWT functions in mock API

---
+++ api/mock-api-const.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN_EXPIRE_IN_SECONDS = 600;
+export const ALL_NUMBERS = '0123456789';
+export const ALL_LETTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const REFRESH_TOKEN_LENGTH = 100;
