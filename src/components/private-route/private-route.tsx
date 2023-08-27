a5e5697d376113bd33891422bc62d5f0b995947a
Eujenio Gonzalez <vegas-host@yandex.ru>
2023-08-27 20:53:15 +0300

    Removed unnecessary redirects in private route

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -3,21 +3,42 @@ import { useNavigate } from 'react-router-dom';
 import { AppRoutes } from '../../const/router-const';
 import { AuthStatuses } from '../../const/common-const';
 import { useAppSelector } from '../../hooks';
-import { getAuthorizationStatus } from '../../store/processes/user-process/user-selectors';
 import { PrivateRouteProps } from '../../types/private-route-props-types';
 
+import {
+  getAuthorizationStatus,
+  getIsUserRequestInProgress
+} from '../../store/processes/user-process/user-selectors';
+import React from 'react';
+import Header from '../header-components/header/header';
+import Footer from '../footer/footer';
+import Content from '../content/content';
+import Loader from '../loader/loader';
+import { LoaderColors, LoaderSizes } from '../../const/loader-const';
+
 function PrivateRoute(props: PrivateRouteProps) {
-  const authStatus = useAppSelector(getAuthorizationStatus);
   const { children } = props;
   const navigate = useNavigate();
+  const authStatus = useAppSelector(getAuthorizationStatus);
+  const isUserRequestInProgress = useAppSelector(getIsUserRequestInProgress);
   
+  const loadingPage = (
+    <>
+      <Header />
+      <Content>
+        <Loader size={LoaderSizes.Large} color={LoaderColors.Grey} />
+      </Content>
+      <Footer />
+    </>
+  );
+
   useEffect(() => {
-    if (authStatus !== AuthStatuses.Auth) {
+    if (authStatus !== AuthStatuses.Auth && !isUserRequestInProgress) {
       navigate(AppRoutes.Login);
     }
   }, [authStatus]);
 
-  return children;
+  return authStatus === AuthStatuses.Auth ? children : loadingPage;
 }
 
 export default PrivateRoute;
